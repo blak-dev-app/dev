c035996c5dd42a4f199e514038b50ce925765bf9
blak-dev-app <saksham.blak@gmail.com>
2026-08-15 15:24:19 +0530

    Create page.tsx

---
+++ apps/web/app/driver/login/page.tsx
@@ -0,0 +1,5 @@
+import { AdminLoginForm } from "@/components/admin/admin-login-form"
+
+export default function DriverLoginPage() {
+  return <AdminLoginForm title="Driver Login" redirectTo="/driver" />
+}
