66f35f2271dfba4129926de1bcff89abf648b406
blak-dev-app <saksham.blak@gmail.com>
2026-08-15 15:25:22 +0530

    Create page.tsx

---
+++ apps/web/app/fleet-onboarding/login/page.tsx
@@ -0,0 +1,5 @@
+import { AdminLoginForm } from "@/components/admin/admin-login-form"
+
+export default function FleetOnboardingLoginPage() {
+  return <AdminLoginForm title="Fleet Partner Login" redirectTo="/fleet-onboarding" />
+}
